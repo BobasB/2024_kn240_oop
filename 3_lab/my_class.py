d2930a22ad95153145564cc10451723757e240ed
bbuhyl <bbuhyl@cisco.com>
2024-10-25 10:57:36 +0300

    пройшли магічні методи

--- 3_lab/my_class.py
+++ 3_lab/my_class.py
@@ -1,4 +1,13 @@
 class MyClass:
+    """Це буде клас для експериментів.
+    Його використовуємо для лаб роботи №3
+
+    ---
+    Attributes:
+    surname: Прізвище
+    name: Імя
+
+    """
     def __init__(self, surname:str, name:str, mark:int): # у конструктор передаються аргументи
         print("Викликали конструктор!")
         # а тут визначаємо атрибути обєкта
@@ -7,11 +16,24 @@ class MyClass:
         self.mark = mark
         #self.penalty = None # можеми записати динамічний атрибут тут, але він перстане бути динамічним
         print("Завершили присвоєння атрибутів")
+        self._test_protected = True
 
+        self.__do_not_touch = "Це змінювати не можна"
+
+    def print_private_attributes(self):
+        return self.__do_not_touch
     
     def __repr__(self):
         return f"MyClass(Ніяких аргументів не переаємо)"
-
+    
+    def __len__(self):
+        return len(self.surname)
+    
+    def __eq__(self, value):
+        if getattr(self, "mark") and getattr(value, "mark", None):
+            return True if self.mark == value.mark else False
+        return None
+        
 class MySecondClass:
     pass
 
