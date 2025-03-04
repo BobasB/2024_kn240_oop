11d1d7dae9117204d6c5012e4efa2532e7ed6b0d
bbuhyl <bbuhyl@cisco.com>
2025-03-04 09:36:50 +0200

    2 парадигма

--- 1_lab/pipenv_env/incapsulation.py
+++ 1_lab/pipenv_env/incapsulation.py
@@ -1,28 +1,4 @@
-class BankAccount:
-    def __init__(self, owner: str, balance: float):
-        self.owner = owner        # Публічний атрибут
-        self.__balance = balance  # Приватний атрибут (інкапсульований)
-    
-    def deposit(self, amount: float):
-        """Додає кошти на рахунок"""
-        if amount > 0:
-            self.__balance += amount
-            print(f"Депозит {amount} успішний. Новий баланс: {self.__balance}")
-        else:
-            print("Сума повинна бути більше 0!")
-
-    def withdraw(self, amount: float):
-        """Знімає кошти з рахунку, якщо баланс дозволяє"""
-        if 0 < amount <= self.__balance:
-            self.__balance -= amount
-            print(f"Знято {amount}. Залишок на рахунку: {self.__balance}")
-        else:
-            print("Недостатньо коштів або некоректна сума!")
-
-    def get_balance(self):
-        """Надає доступ до балансу (через метод, а не напряму)"""
-        return self.__balance
-
+from bank import BankAccount
 
 # Демонстрація роботи
 account = BankAccount("Іван", 1000)
