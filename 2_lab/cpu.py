3a4871339d8115f05a06d480fe0e84f4cdaf0e77
bbuhyl <bbuhyl@cisco.com>
2025-04-08 09:43:12 +0300

    пройшли processPool

--- 2_lab/cpu.py
+++ 2_lab/cpu.py
@@ -1,11 +1,10 @@
 from multiprocessing import cpu_count
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
 from threading import current_thread
-from time import sleep
+from time import sleep, time
 import math
+import numpy as np
 
-n_cpu = cpu_count()
-print(f"Кількість процесорів на даному компютері: {n_cpu}")
 
 def check_thread(v):
     """Функція для багатопоточночті через треди
@@ -14,22 +13,50 @@ def check_thread(v):
     print(f"{v}: імя потоку={thread.name}")
     a = math.sqrt(64^v)
 
-with ThreadPoolExecutor() as executor:
-    executor.map(check_thread, range(n_cpu))
+#with ThreadPoolExecutor() as executor:
+#    executor.map(check_thread, range(n_cpu))
 
 
 def is_prime(n):
     """Перевірка, чи є число простим."""
+    print(f"Починаємо визначати чи число {n} є простим.")
     if n <= 1:
-        return False
+        return (n, False)
     if n == 2:
-        return True
+        return (n, True)
     if n % 2 == 0:
-        return False
+        return (n, False)
     sqrt_n = int(math.sqrt(n)) + 1
     for i in range(3, sqrt_n, 2):
         if n % i == 0:
-            return False
-    return True
+            return (n, False)
+    return (n, True)
 
-print("Завершення")
+def gen_matrix(n):
+    print(f"Генеруємо матрицю {n}x{n}")
+    start = time()
+    m = np.random.rand(n, n)
+    end = time()
+    print(f"Матриця {m.ndim} згенерувалась за {end - start}")
+    return (n, True)
+
+def compute_():
+    for n in range(100):
+        if is_prime(n):
+            print("Так просте")
+        else:
+            print("Ні не просте")
+
+if __name__ == '__main__':
+    n_cpu = cpu_count()
+    print(f"Кількість процесорів на даному компютері: {n_cpu}")
+    with ProcessPoolExecutor() as executor:
+        #futures = executor.map(is_prime, range(100))
+        futures = executor.map(gen_matrix, range(1000, 1100))
+        for num, is_prime_result in futures:
+            if is_prime_result:
+                print(f"Число {num} є простим")
+            else:
+                print(f"Число {num} Не є простим")
+
+    print("Завершення")
