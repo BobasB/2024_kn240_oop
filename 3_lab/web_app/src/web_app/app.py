a13e1d6d3b8f1c3c7b6dc9ecd78ea8ea1e4de2aa
bbuhyl <bbuhyl@cisco.com>
2025-05-01 09:42:08 +0300

    working app

--- 3_lab/web_app/src/web_app/app.py
+++ 3_lab/web_app/src/web_app/app.py
@@ -3,7 +3,6 @@ from redis import Redis
 
 app = Flask(__name__)
 r = Redis(host='localhost', port=6379, db=0)
-C = 0
 
 @app.route("/")
 def hello():
@@ -11,10 +10,14 @@ def hello():
 
 @app.route("/info")
 def info():
-    C += 1
-    r.set("info_count", C)
+    C = r.incr("info_count")
     return f"Інкрементуємо значення до {C}"
+
+@app.route("/del")
+def del_info():
+    C = r.decr("info_count")
+    return f"Декрементуємо значення до {C}"
 
 if __name__ == "__main__":
     # Runs the app on localhost at port 5000 in debug mode
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="localhost", port=5000, debug=True)
